9fe54a7f03dfb3c9e501787a000b7a75b82059b2
Carlos Torres <ingcarlostorres@outlook.com>
2024-02-13 21:11:18 +0100

    Se modifica archivo app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let textEncriptar="";
 
 function Encriptar() {
-    
+    let letras;
+    let letraA;
     textEncriptar= document.getElementById('TextoEncriptar').value;
     console.log(textEncriptar);
     
@@ -14,7 +15,18 @@ function Encriptar() {
         document.getElementById('imagen').style.display ='none';
         document.getElementById('mensajeTexto').style.display='none';
     }
+
+    letras = textEncriptar.split('');
+    //Con esta sentencia busca en el arreglo si hay una letra i, devuelve un tru si cumple la condicion o un false si no
+    //letraA = letras.includes('i');
     
+    letraA = letras.filter(elemento=> elemento =='i')
+    console.log(letras);
+    console.log(letraA);
+
+
+
+
     
 }
 
