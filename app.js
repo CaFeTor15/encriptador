e4765288f0a4b8388ef766ff87c7ed84134abdc5
Carlos Torres <ingcarlostorres@outlook.com>
2024-02-14 22:04:09 +0100

    Se  crearon las funciones de encriptacion y desencriptacion

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-let textEncriptar="";
+let textEncriptar=[];
+let llavesObj ={
+    a:'ai',
+    e:'enter',
+    i:'imes',
+    o:'ober',
+    u:'ufat'
+
+};
+
+let llavesDesencrpObj={
+    ai:'a',
+    enter:'e',
+    imes:'i',
+    ober:'o',
+    ufat:'u'
+}
 
 function Encriptar() {
-    let letras;
-    let letraA;
-    textEncriptar= document.getElementById('TextoEncriptar').value;
-    console.log(textEncriptar);
-    
-    //Controla si el usiario ingresa el Texto a desencriptar mostrando una imagen y un mensaje
-    if(textEncriptar==""){
+    let texEncriptado;
+   //Controla si el usiario ingresa el Texto a desencriptar mostrando una imagen y un mensaje
+   if(textEncriptar==""){
         document.getElementById('imagen').style.display ='block';
         document.getElementById('mensajeTexto').style.display='block';
     }
@@ -15,19 +27,18 @@ function Encriptar() {
         document.getElementById('imagen').style.display ='none';
         document.getElementById('mensajeTexto').style.display='none';
     }
-
-    letras = textEncriptar.split('');
-    //Con esta sentencia busca en el arreglo si hay una letra i, devuelve un tru si cumple la condicion o un false si no
-    //letraA = letras.includes('i');
-    
-    letraA = letras.filter(elemento=> elemento =='i')
-    console.log(letras);
-    console.log(letraA);
-
-
-
-
-    
+    textEncriptar= document.getElementById('TextoEncriptar').value;
+    console.log(textEncriptar);
+    texEncriptado=textEncriptar.replace(/a|e|i|o|u/g,function(matched) {
+        return llavesObj[matched];});
+        console.log(texEncriptado);
 }
 
+function Desencriptar(){
+    let textoDesencriptado;
+    textEncriptar= document.getElementById('TextoEncriptar').value;
+    textoDesencriptado=textEncriptar.replace(/ai|enter|imes|ober|ufat/g,function(matched) {
+        return llavesDesencrpObj[matched];});
+        console.log(textoDesencriptado);
+}
 
